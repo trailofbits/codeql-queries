e4e3042e14da91dae3a7534e3429be2914322328
GrosQuildu <e2.8a.95@gmail.com>
2024-11-15 16:16:45 +0100

    java recursion more tests

--- java/test/query-tests/security/Recursion/Recursion.java
+++ java/test/query-tests/security/Recursion/Recursion.java
@@ -1,4 +1,72 @@
-class RecursiveCall { 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+class StreamException extends RuntimeException {
+    public StreamException(Throwable cause) {
+        super(cause);
+    }
+}
+
+class Token {
+    public static final int TYPE_MAP_ID_TO_VALUE = 0x2;
+    
+    private int type;
+    private String id;
+    private Object value;
+    
+    public int getType() {
+        return type;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public Object getValue() {
+        return value;
+    }
+}
+
+class TokenFormatter {
+    public Token read(InputStreamReader in) throws IOException {
+        // Implementation would go here
+        return new Token();
+    }
+}
+
+class RecursiveCallExample {
+    private TokenFormatter tokenFormatter = new TokenFormatter();
+    private Map<String, Object> idRegistry = new HashMap<>();
+    private InputStreamReader in;
+
+    public RecursiveCallExample(InputStreamReader in) {
+        this.in = in;
+    }
+
+    // finding: readToken calls itself
+    // Based on https://github.com/x-stream/xstream/blob/dfa1d35462fe84412ee72a9b0cf5b5c633086520/xstream/src/java/com/thoughtworks/xstream/io/binary/BinaryStreamReader.java#L165
+    Token readToken() {
+        // ...
+        try {
+            final Token token = tokenFormatter.read(in);
+            switch (token.getType()) {
+            case Token.TYPE_MAP_ID_TO_VALUE: // 0x2
+                idRegistry.put(token.getId(), token.getValue());
+                return readToken(); // Next one please.
+            default:
+                return token;
+            }
+        } catch (final IOException e) {
+            throw new StreamException(e);
+        }
+        // ...
+    }
+}
+
+class RecursiveCallBasic {
+    // finding: foo-bar recursive loop
     public boolean bar() {
         boolean fooResult = foo();
         return fooResult;
@@ -8,10 +76,12 @@ class RecursiveCall {
         return bar();
     }
 
+    // finding: calls to self
     public boolean directRecursive() {
         return directRecursive();
     }
 
+    // finding: level0->level1->level2->level0
     public boolean level0() {
         if (someCondition()) {
             return true;
@@ -34,11 +104,57 @@ class RecursiveCall {
     private boolean someCondition() {
         return false;
     }
- }
+}
+
+class RecursiveCallWronglyLimited {
+    // finding: recursion is not limited
+    public boolean directRecursiveNoDepth(int anything, int depth) {
+        if (depth == 0) {
+            return true;
+        }
+        return directRecursiveNoDepth(anything, depth);
+    }
+}
+
+class RecursiveCallLimited {
+    // todook: recursion is limited
+    public boolean directRecursiveDepth(int depth) {
+        if (depth == 0) {
+            return true;
+        }
+        return directRecursiveDepth(depth - 1);
+    }
+
+    // todook: level0->level1->level2->level0 with bound
+    public boolean level0D(int depth) {
+        if (depth == 0) {
+            return true;
+        }
+        if (someCondition()) {
+            return true;
+        }        
+        return level1D(depth-1);
+    }
+    public boolean level1D(int depth) {
+        if (someCondition()) {
+            return true;
+        }        
+        return level2D(depth);
+    }
+    public boolean level2D(int depth) {
+        if (someCondition()) {
+            return true;
+        }
+        return level0D(depth-1);
+    }
+
+    private boolean someCondition() {
+        return false;
+    }
+}
  
-
+// ok
 class NotRecursive {
-
     public static boolean foo() {
         return bar();
     }
